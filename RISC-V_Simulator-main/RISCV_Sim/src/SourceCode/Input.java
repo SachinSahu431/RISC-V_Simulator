f29ce480bfd480d5ca5f095d9361e681b2628dd2
Samveg-techie <shahsamveg2003@gmail.com>
2022-03-01 18:05:59 +0530

    update in memory class

--- RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Input.java
+++ RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Input.java
@@ -1,13 +1,9 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+1. Take input as words and then do the operations on the corresponding data
+*/
+
 package SourceCode;
 
-/**
- *
- * @author shahs
- */
 public class Input {
-    
+
 }
--- RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Memory.java
+++ RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Memory.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package SourceCode;
 
-/**
- *
- * @author shahs
- */
 public class Memory {
     
+    int[] Register = new int[32]; //All required 32 registers
+    // Memory should be of 4kb => 4000 Bytes
+    //In our system 1 word corr. to 4 bytes of memory
+    int[] Memory = new int[1000]; //In words i.e. 4 bytes
+    
+    //Constructor to initialise all of it's stuffs
+    Memory(){
+        
+    }
+    
+    // Non-Arithmetic Operations
+    void loadWord(int regIndex, int memIndex){
+        this.Register[regIndex] = Memory[memIndex];
+    }
+    
+    void storeWord(int regIndex, int memIndex){
+        Memory[memIndex] = Register[regIndex];
+    }
+    
+    void branchNotEqual(){
+        
+    }
+    
+    void jumpAndLink(){
+        
+    }
 }
