93621e8bcdbfd3cef77966f1a07bd1cef4092a45
Samveg-techie <shahsamveg2003@gmail.com>
2022-03-02 19:25:33 +0530

    Change in memory

--- RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Memory.java
+++ RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Memory.java
@@ -1,31 +1,31 @@
 package SourceCode;
 
 public class Memory {
-    
-    int[] Register = new int[32]; //All required 32 registers
+
+    int[] Register = new int[32]; // All required 32 registers
     // Memory should be of 4kb => 4000 Bytes
-    //In our system 1 word corr. to 4 bytes of memory
-    int[] Memory = new int[1000]; //In words i.e. 4 bytes
-    
-    //Constructor to initialise all of it's stuffs
-    Memory(){
-        
+    // In our system 1 word corr. to 4 bytes of memory
+    int[] Memory = new int[1000]; // In words i.e. 4 bytes
+
+    // Constructor to initialise all of it's stuffs
+    Memory() {
+
     }
-    
+
     // Non-Arithmetic Operations
-    void loadWord(int regIndex, int memIndex){
+    void loadWord(int regIndex, int memIndex) {
         this.Register[regIndex] = Memory[memIndex];
     }
-    
-    void storeWord(int regIndex, int memIndex){
+
+    void storeWord(int regIndex, int memIndex) {
         Memory[memIndex] = Register[regIndex];
     }
-    
-    void branchNotEqual(){
-        
+
+    void branchNotEqual() {
+
     }
-    
-    void jumpAndLink(){
-        
+
+    void jumpAndLink() {
+
     }
 }
