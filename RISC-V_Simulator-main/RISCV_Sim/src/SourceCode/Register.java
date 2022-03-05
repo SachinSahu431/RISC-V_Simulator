97e09498b9d2853ceb30f49454f464f27456a906
Sachin Sahu <sachinksahu.431@gmail.com>
2022-03-05 12:19:26 +0530

    Improve 'Print registers'

--- RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Register.java
+++ RISC-V_Simulator-main/RISCV_Sim/src/SourceCode/Register.java
@@ -26,12 +26,13 @@ public class Register extends Memory {
     // print all registers
     public void printRegisters() {
         System.out.println("Register Values:");
-        String format = "%-5s %5d%15s%20s\n";
+        String format = "%-2s %2s%8d%15s%20s\n";
         System.out.println("\n\nRegister Name \t Decimal \t Hexadecimal \t Binary");
 
         for (int i = 0; i < 32; i++) {
             System.out.printf(format,
-                    "Register[" + i + "]: ", this.Register[i],
+                    "Register[" + i + "]: ", RegisterName[i],
+                    this.Register[i],
                     Integer.toHexString(this.Register[i]),
                     Integer.toBinaryString(this.Register[i]));
         }
@@ -50,4 +51,9 @@ public class Register extends Memory {
         System.out.println("Registers Cleared");
     }
 
+    // register and symbolic names
+    public static final String[] RegisterName = {
+            "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6",
+            "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6", "pc"
+    };
 }
